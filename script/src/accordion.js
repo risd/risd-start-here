75c6062aa2df72630d2483b1af73d82691006c45
mmarol <mmarol01@risd.edu>
2019-06-28 12:34:08 -0400

    added basic accordion functionality

---
+++ script/src/accordion.js
@@ -0,0 +1,15 @@
+var $ = global.jQuery;
+
+module.exports = accordion;
+
+function accordion() {
+  if (!(this instanceof accordion)) {
+    return new accordion();
+  }
+  // console.log('activeNav initialized.');
+
+  $('.question').click(function(){
+    console.log('clicked!');
+    $(this).siblings('.answer').toggleClass('show');
+  });
+}
--- script/src/index.js
+++ script/src/index.js
@@ -1,1 +1,3 @@
 global.jQuery = require("jquery");
+
+var accordion = require('./accordion.js')();
