ab66e681a2bbe2a5acabebc9de4025bf357e4357
rubonics <rubenrodriguez+github@gmail.com>
2019-07-24 18:32:53 -0400

    [script] adds set-selector-style-on-load usage to script bundle root

--- script/src/index.js
+++ script/src/index.js
@@ -1,3 +1,9 @@
 global.jQuery = require("jquery");
 
 var accordion = require('./accordion.js')();
+var SetIFramePositionRelative = require( './set-selector-style-on-load.js' )( {
+  selector: 'iframe.instagram-media',
+  styles: {
+    position: 'relative',
+  }
+} )
