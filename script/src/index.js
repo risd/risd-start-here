52c9939a053580808a5441f7c5b9994fe72d7b4d
rubonics <rubenrodriguez+github@gmail.com>
2019-07-25 14:29:35 -0400

    [script] adds slick slider initializer. also removes commented out code

--- script/src/index.js
+++ script/src/index.js
@@ -1,10 +1,6 @@
 global.jQuery = require("jquery");
 
 var accordion = require('./accordion.js')();
-// too inconsistent, leaning toward an !important style
-// var SetIFramePositionRelative = require( './set-selector-style-on-load.js' )( {
-//   selector: 'iframe.instagram-media',
-//   styles: {
-//     position: 'relative',
-//   }
-// } )
+var sliders = require('./sliders.js')( {
+  slider: 'slider-container',
+} )
