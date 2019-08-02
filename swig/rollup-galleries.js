30557468531a3e1116355b2f0d8a4125228b5c04
rubonics <rubenrodriguez+github@gmail.com>
2019-08-01 20:20:02 -0400

    reverts to using alt instagram embed script. probably gonna try getting this done via the instagram embed api

--- swig/rollup-galleries.js
+++ swig/rollup-galleries.js
@@ -29,7 +29,7 @@ slider galleries
 
 function rollup_galleries ( input ) {
   // ensure we are using the layout bug free version of the instagram embed
-  // input = input.replace( /www.instagram.com\/embed.js/g, 'platform.instagram.com/en_US/embeds.js' )
+  input = input.replace( /www.instagram.com\/embed.js/g, 'platform.instagram.com/en_US/embeds.js' )
   
   // roll up sliders
   var parseRootId = 'parse-root'
