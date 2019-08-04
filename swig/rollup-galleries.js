62ef7a02667b6e5b9bf722423b331dd243ae0f5b
rubonics <rubenrodriguez+github@gmail.com>
2019-08-03 21:22:15 -0400

    removes redundant instagram embed script tags

--- swig/rollup-galleries.js
+++ swig/rollup-galleries.js
@@ -1,8 +1,13 @@
 var cheerio = require('cheerio')
-var count = 0
+// this is flipped to true if there is an instagram
+// embed script that is processed
+var insert_instagram_embed =  false
+var instagram_embed_url = 'www.instagram.com/embed.js'
+// var instagram_embed_url = 'platform.instagram.com/en_US/embeds.js'
 
 module.exports = {
   rollup_galleries: rollup_galleries,
+  optional_instagram_embed: optional_instagram_embed,
 }
 
 /*
@@ -12,24 +17,71 @@ as input, and rolls up consective
 top level `figure` elements into
 slider galleries
 
+sample input:
 <p></p>
 <p></p>
 <h3></h3>
-<figure></figure> - data-slider-0
-<figure></figure> - data-slider-0
-<figure></figure> - data-slider-0
+<figure></figure>
+<figure></figure>
+<figure></figure>
 <h3></h3>
 <p></p>
-<figure></figure> - data-slider-1
-<figure></figure> - data-slider-1
-<figure></figure> - data-slider-1
+<figure></figure>
+<figure></figure>
+<figure></figure>
 <p></p>
+
+
+with data-slider attributes:
+<p></p>
+<p></p>
+<h3></h3>
+<figure data-slider=0></figure>
+<figure data-slider=0></figure>
+<figure data-slider=0></figure>
+<h3></h3>
+<p></p>
+<figure data-slider=1></figure>
+<figure data-slider=1></figure>
+<figure data-slider=1></figure>
+<p></p>
+
+wrapped in slider marker:
+<p></p>
+<p></p>
+<h3></h3>
+<ul class="gallery__slider" data-slider=0>
+  <li class="slide">
+    <figure data-slider=0></figure>
+  </li>
+  <li class="slide">
+    <figure data-slider=0></figure>
+  </li>
+  <li class="slide">
+    <figure data-slider=0></figure>
+  </li>
+</ul>
+<h3></h3>
+<p></p>
+<ul class="gallery__slider" data-slider=1>
+  <li class="slide">
+    <figure data-slider=1></figure>
+  </li>
+  <li class="slide">
+    <figure data-slider=1></figure>
+  </li>
+  <li class="slide">
+    <figure data-slider=1></figure>
+  </li>
+</ul>
+<p></p>
+
 
  */
 
 function rollup_galleries ( input ) {
   // ensure we are using the layout bug free version of the instagram embed
-  input = input.replace( /www.instagram.com\/embed.js/g, 'platform.instagram.com/en_US/embeds.js' )
+  // input = input.replace( /${ instagram_embed_url.replace( /\//g, '\/' ) }/g, 'platform.instagram.com/en_US/embeds.js' )
   
   // roll up sliders
   var parseRootId = 'parse-root'
@@ -81,7 +133,7 @@ function rollup_galleries ( input ) {
     slider.previous = element;
   } )
 
-  // nest sliders within a slider-container
+  // nest sliders within their container markup
   for (var i = 0; i <= slider.counter; i++) {
     var sliderQuery = `figure[${ trackingAttr }=${ i }]`
     var sliderContents = $( sliderQuery )
@@ -99,7 +151,25 @@ function rollup_galleries ( input ) {
     $( parseRootQuery ).children( sliderQuery ).remove()
   }
 
+  check_remove_instagram_embed_script( $( parseRootQuery ) )
+
   var output = $( parseRootQuery ).html()
 
   return output
+}
+
+function check_remove_instagram_embed_script ( $root ) {
+  var $scripts = $root.find( `script[src="//${ instagram_embed_url }"]` )
+  if ( $scripts.length > 0 ) {
+    insert_instagram_embed = true;
+    $scripts.remove()
+  }
+}
+
+function optional_instagram_embed () {
+  if ( insert_instagram_embed === false) return ''
+  else {
+    return `
+      <script async src="//${ instagram_embed_url }"></script>`
+  }
 }
