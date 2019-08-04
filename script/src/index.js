17856660916949445bfa5f9cb92bdade5ffd549d
rubonics <rubenrodriguez+github@gmail.com>
2019-08-03 22:34:22 -0400

    [script] comments out responsive sizing rules

--- script/src/index.js
+++ script/src/index.js
@@ -10,41 +10,42 @@ var sliders = require('./sliders.js')( {
   slick: {
     autoplay: false,
     lazyLoad: 'ondemand',
-    slidesToShow: 1,
-    centerPadding: '40px',
+    // slidesToShow: 1,
+    // centerPadding: '40px',
     centerMode: true,
     infinite: true,
     mobileFirst: true,
+    adaptiveHeight: true,
     prevArrow: `<button class="gallery__arrows gallery__previous">◀</button>`,
     nextArrow: `<button class="gallery__arrows gallery__next">▶</button>`,
-    responsive: [
-      {
-        breakpoint: 326,
-        settings: {
-          arrows: true,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1,
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: true,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 2,
-        }
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          arrows: true,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 2,
-        }
-      },
-    ],
+    // responsive: [
+    //   {
+    //     breakpoint: 326,
+    //     settings: {
+    //       arrows: true,
+    //       centerMode: true,
+    //       centerPadding: '40px',
+    //       slidesToShow: 1,
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 768,
+    //     settings: {
+    //       arrows: true,
+    //       centerMode: true,
+    //       centerPadding: '40px',
+    //       slidesToShow: 2,
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 1024,
+    //     settings: {
+    //       arrows: true,
+    //       centerMode: true,
+    //       centerPadding: '40px',
+    //       slidesToShow: 2,
+    //     }
+    //   },
+    // ],
   }
 } )
