973147dcf7ad874a58d79f8c7f17424c3427c75e
rubonics <rubenrodriguez+github@gmail.com>
2019-08-11 13:21:25 -0400

    [tasks] updates build-all for postcss transition

--- tasks/build-all.js
+++ tasks/build-all.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
   grunt.registerTask( 'build-all', 'Build JS, CSS, templates & pages.', function () {
     grunt.task.run( 'build' )
     grunt.task.run( 'sass' )
-    grunt.task.run( 'autoprefixer' )
+    grunt.task.run( 'postcss' )
     grunt.task.run( 'browserify:client' )
     grunt.task.run( 'build-static' )
   })
