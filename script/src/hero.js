2bf1120ddfef81f54e2f76616b571fb14f9c0816
rubonics <rubenrodriguez+github@gmail.com>
2019-08-11 12:00:08 -0400

    updates hero animation handling

--- script/src/hero.js
+++ script/src/hero.js
@@ -13,11 +13,14 @@ function Hero(opts) {
   var $text = $( '.hero__text-container' )
 
   // set the initial position of the hero text,
-  // so that it can slide in
+  // so that it can slide in from a consistent position
+  // relative to the screen size
+  var textTransformYStart = ( $hero.outerHeight() - $text.outerHeight() ) / 2 + $text.outerHeight()
+
   $text
     .get( 0 )
     .style
-    .setProperty( '--transform-start', `translate(0, ${ $hero.outerHeight() - $text.outerHeight() }px) rotate(0deg)` )
+    .setProperty( '--transform-start', `translate(0, ${ textTransformYStart }px) rotate(0deg)` )
 
   setTimeout( show, animationTimeout )
 
